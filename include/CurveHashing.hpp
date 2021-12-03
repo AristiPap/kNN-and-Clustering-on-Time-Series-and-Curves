77e54c6ea95622f1b2516f7e2b60a42f3eab7583
AristiPap <sdi1800154@di.uoa.gr>
2021-12-03 13:10:15 +0200

    Initial plan for Curve Hashing

---
+++ include/CurveHashing.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <list>
+#include <random>
+#include <set>
+#include <unordered_map>
+#include <vector>
+
+#include "Point.hpp"
+#include "Utilities.hpp"
+#include "global_variables_namespace.hpp"
+
+#define BIGINT 4294967291
+// Basic Hashing interface
+class HashingCurve {
+   protected:
+    int dim;  // Dimension of the points of the curves
+    double delta;
+    int L;  // ammout of grids
+    int w;
+    int k;
+
+   public:
+    // every hashing subclass must implement the () operator overloading to
+    // enable hashing activity
+    virtual uint32_t operator()(const Point& p) = 0;
+    HashingCurve(int32_t k, int32_t w, int32_t dim);
+    ~HashingCurve();
+};
+
+// Hashing class for LSH algorithm
+class DLSHHashingCurve : HashingCurve {
+   private:
+    vector<double> t;
+
+   public:
+    uint32_t operator()(const Point& p);
+    DLSHHashingCurve(int32_t k, int32_t w, int32_t dim);
+    ~DLSHHashingCurve();
+    static uint32_t estimate_w(std::list<Point*>& dataset);
+};
+
+class CLSHHashingCurve : HashingCurve {
+   private:
+    vector<double> t;
+    
+   public:
+    uint32_t operator()(const Point& p);
+    CLSHHashingCurve(int32_t k, int32_t w, int32_t dim);
+    ~CLSHHashingCurve();
+    static uint32_t estimate_w(std::list<Point*>& dataset);
+};
