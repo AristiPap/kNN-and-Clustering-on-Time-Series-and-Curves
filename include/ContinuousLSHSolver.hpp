dc4cc82a069c9c3532b7f20de58c7fbdcf451501
VissaMoutafis <sdi1800119@di.uoa.gr>
2021-12-03 14:52:58 +0200

    changes in hpp's of solvers

--- include/ContinuousLSHSolver.hpp
+++ include/ContinuousLSHSolver.hpp
@@ -1,20 +1,20 @@
-#pragma once
+#pragma once 
 
 #include "CurveNearestNeighbours.hpp"
 
-class DiscreteLSHSolver: public CurveNearestNeighboursSolver{
+class ContinuousLSHSolver: public CurveNearestNeighboursSolver{
 private:
-    std::vector<DLSHHashingCurve> hs; // use the grid snappers, h's, to transform the curve into a vector 
+
+    // TODO: add filtering of the curve either out or in this class
+    std::vector<CLSHHashingCurve> hs; // use the grid snappers, h's, to transform the curve into a vector 
     std::vector<NearestNeighboursSolver> solvers; // use one different solver for each h to estimate proximity between the grid solvers
 
 public:
-    DiscreteLSHSolver(std::list<Curve *> &dataset, uint32_t curve_L, uint32_t curve_delta, uint32_t curve_d);
-    ~DiscreteLSHSolver();
+    ContinuousLSHSolver(std::list<Curve *> &dataset, uint32_t curve_L, uint32_t curve_delta, uint32_t curve_d);
+    ~ContinuousLSHSolver();
     // Find N nearest neighbours of q. Returns list of <Curve *, dist from q>.
     std::list<CurveNeighbour> *kNearestNeighbours(const Curve &q, uint N = 1) final;
     // Find all neighbours in range R. Returns list of <Curve *, dist from q>.
     std::list<CurveNeighbour> *nearestNeighbours_w_rangeSearch(const Curve &q, double R = 10000.0) final;
 
 };
-
-
---
+++ include/CurveNearestNeighbours.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+
+#include "CurveHashing.hpp"
+#include "Curves.hpp"
+#include "NearestNeighboursSolver.hpp"
+#include "Point.hpp"
+#include "Utilities.hpp"
+#include "frechet.hpp"
+
+using CurveNeighbour = std::pair<Curve *, double>;
+
+class CurveNearestNeighboursSolver {
+   private:
+    uint32_t _curve_L;
+    uint32_t _curve_delta;
+    uint32_t _curve_d;
+    std::list<Curve *> &dataset;
+
+   public:
+    CurveNearestNeighboursSolver(std::list<Curve *> &dataset, uint32_t curve_L,
+                                 uint32_t curve_delta, uint32_t curve_d);
+    ~CurveNearestNeighboursSolver();
+
+    // Find N nearest neighbours of q. Returns list of <Curve *, dist from q>.
+    virtual std::list<CurveNeighbour> *kNearestNeighbours(const Curve &q,
+                                                          uint N = 1) = 0;
+    // Find all neighbours in range R. Returns list of <Curve *, dist from q>.
+    virtual std::list<CurveNeighbour> *nearestNeighbours_w_rangeSearch(
+        const Curve &q, double R = 10000.0) = 0;
+};
